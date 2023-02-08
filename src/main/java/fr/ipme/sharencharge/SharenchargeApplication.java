12370f9463b0cfca6a72d95bae23c016e738127d
Benoit CROZEMARIE <benoit.crozemarie@gmail.com>
2023-02-08 11:32:54 +0100

    first commit !

---
+++ src/main/java/fr/ipme/sharencharge/SharenchargeApplication.java
@@ -0,0 +1,13 @@
+package fr.ipme.sharencharge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharenchargeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharenchargeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ipme/sharencharge/SharenchargeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ipme.sharencharge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharenchargeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
